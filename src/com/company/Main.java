0c39029779a97af6dd32f5ea8bf0132aa3c0c047
Sheetal Samnani <ukssam@bmi03985.local>
2021-09-18 10:35:48 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 20;
+        int c = a + b;
+        int d = a -b;
+        System.out.println(c);
+        System.out.println(d);
+	// write your code here
+    }
+}
